52b0121361955d25902d520a7a74813cdc038042
Fabio Rotondo <fabio.rotondo@gmail.com>
2019-03-05 03:34:06 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,50 @@
+# express-dump-curl
+
+This `ExpressJS` middleware dumps the equivalent curl command of the request received.
+
+The `curl` dump can be written in standard output or on a specific file and it is generally very well formatted to ease both reading and editing.
+
+
+## Installation
+Install the package by running one of the following commands, depending on your configuration:
+
+```npm install --save express-dump-curl```
+
+```yarn add express-dump-curl```
+
+
+## Usage
+1. Include the package in your ExpressJS app along with other dependencies.
+
+```
+const fs = require ( 'fs' );
+const express = require ( 'express' );
+
+// Include express-dump-curl as a dependency
+const dump_curl = require ( 'express-curl' );
+
+const app = express ();
+...
+```
+
+2. Declare this as a middleware just above the route definitions
+
+```
+app.use ( bodyParser.json() );
+app.use ( multiplart() );
+...
+
+// Include the express dump curl middleware
+// if no filename is specified in the call, console.log will be
+// used to dump curls
+app.use ( dump_curl () );
+
+// Alternatively, you can specify a filename in dump_curl definition
+// app.use ( dump_curl ( "/ramdisk/curls.txt" ) );
+
+...
+app.use('/auth', authRouter);
+app.use('/users', usersRouter);
+```
+
+From now on, every single call will be logged on console or file depending on your configuration.
---
+++ index.js
@@ -0,0 +1,60 @@
+/*
+ * dump_curl v 0.1 - By Fabio Rotondo (fabio.rotondo@gmail.com)
+ * 
+ * Inspired by: https://github.com/sahilnarain/express-curl
+*/
+const fs = require ( "fs" );
+
+const _build = ( params ) =>
+{
+	const _headers = [];
+	const _body = [];
+
+	try {
+		if ( params.headers ) 
+		{
+			if ( params.headers [ 'content-length' ] ) delete params.headers [ 'content-length' ];
+
+			for ( let key in params.headers ) _headers.push ( `-H '${key}:${params.headers[key]}'` );
+		}
+
+
+		if ( params.body ) 
+		{
+			if ( params.headers && params.headers [ 'content-type' ] && params.headers [ 'content-type' ] === 'application/json' )
+			{
+				_body.push ( `-d '${JSON.stringify(params.body)}'` );
+			} else {
+				for ( let key in params.body ) _body.push ( `-d '${key}=${params.body[key]}'` );
+			}
+		}
+
+		const heads = _headers.join ( " \\\n " );
+		const body  = _body.join ( " \\\n " );
+
+		return `curl -X ${params.verb.toUpperCase()} \\\n ${heads} \\\n ${body} \\\n '${params.url}'\n\n\n`;
+	} catch ( e ) {
+		console.error ( e );
+	}
+};
+
+const _dump_curl = function ( output_fname, req, res, next ) 
+{
+	var params = {};
+
+	params.url  = req.protocol + '://' + ( req.headers.host || req.hostname ) + req.originalUrl;
+	params.verb = req.method.toUpperCase();
+	req.headers ? params.headers = req.headers : null;
+	req.body ? params.body = req.body : null;
+
+	if ( ! output_fname ) 
+		console.log ( `${_build(params)}` );
+	else
+		fs.appendFileSync ( output_fname, `${_build(params)}` );
+
+	next();
+};
+
+const dump_curl = ( output_fname ) => ( req, res, next ) => _dump_curl ( output_fname, req, res, next );
+
+module.exports = dump_curl;
